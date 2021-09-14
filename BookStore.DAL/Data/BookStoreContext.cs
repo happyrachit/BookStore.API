e13e3b46203507015bfe1a445be6c19d040b17bb
happyrachit <53208387+happyrachit@users.noreply.github.com>
2021-09-14 20:27:13 +0530

    Add project files.

---
+++ BookStore.DAL/Data/BookStoreContext.cs
@@ -0,0 +1,21 @@
+﻿using BookStore.Models.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DAL.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Books> Books { get; set; }
+    }
+}
